b920d12fc5ad873ca4c1c999bf0f955aef43ec50
Le Van Thuan <thuanat@gmail.com>
2024-02-21 12:22:41 +0700

    git init

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+
+Route::get('/', function () {
+    return view('layout');
+});
+Route::resource("/students", StudentController::class);
